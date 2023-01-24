be0906b89ae7e7dca9a452f2b7a191119563168e
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2023-01-24 18:52:42 +0530

    making api call on render backend

--- client/src/constants/constants.js
+++ client/src/constants/constants.js
@@ -1,5 +1,5 @@
 import io from "socket.io-client";
 
 // export const URL = 'http://localhost:5000';
-export const URL = "";
+export const URL = "https://project-management-web-app.onrender.com";
 export const socket = io(`${URL}`);
